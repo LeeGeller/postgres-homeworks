ebe11fbdf0163f74f46a8d4ceddf13faa7e1138c
Kristina Arkadievna <108188624+LeeGeller@users.noreply.github.com>
2024-02-21 22:38:52 +0300

    getenv

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
 import pathlib
 
 ROOT = pathlib.Path(__file__).parent
 DATA_EMPLOYEES = pathlib.Path(ROOT, 'homework-1', 'north_data', 'employees_data.csv')
 
+PASS = os.getenv('Password')
