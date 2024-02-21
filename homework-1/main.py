9b50410ee9de7241fba04bb45c1e661e3627a143
Kristina Arkadievna <108188624+LeeGeller@users.noreply.github.com>
2024-02-21 22:58:25 +0300

    Add docstrings

--- homework-1/main.py
+++ homework-1/main.py
@@ -6,6 +6,13 @@ from config import DATA_EMPLOYEES, PASS, DATA_CUSTOMERS, DATA_ORDERS
 
 
 def open_data_file(data) -> list:
+    """
+    Открывает файл csv. Перебирает его и создает список из словарей.
+    Т.к. в БД в колонке employee_id должен быть тип int,
+    меняем тип в row по ключу 'employee_id'.
+    :data: путь до файла
+    :return: список из словарей
+    """
     new_list = []
 
     with open(data, encoding='utf-8') as file:
@@ -18,6 +25,12 @@ def open_data_file(data) -> list:
 
 
 def add_data_to_database(data_list: list, name_table: str, column_count: int) -> None:
+    """
+    Отправляет информацию в БД.
+    :data_list: список из словарей, заполненный данными
+    :name_table: название таблицы, куда отправляется инфо
+    :column_count: количество столбцов в этой таблице
+    """
     count = '%s ' * column_count
     with psycopg2.connect(
             host='localhost',
