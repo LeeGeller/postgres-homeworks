7bc0413b30ffc485ab4e683109844b0609cbc550
Kristina Arkadievna <108188624+LeeGeller@users.noreply.github.com>
2024-02-21 22:39:10 +0300

    Created open_data_file() and add_data_to_database()

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,14 +2,38 @@ import csv
 
 import psycopg2
 
-from config import DATA_EMPLOYEES
-
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+from config import DATA_EMPLOYEES, PASS
 
 
-def open_data_file(data):
+def open_data_file(data) -> list:
+    new_list = []
+
     with open(data, encoding='utf-8') as file:
-        return csv.DictReader(file.read())
+        csv_file = csv.DictReader(file)
+        for row in csv_file:
+            try:
+                row['employee_id'] = int(row['employee_id'])
+            except:
+                continue
+            new_list.append(row)
+    return new_list
 
 
-print(open_data_file(DATA_EMPLOYEES))
+def add_data_to_database(data_list: list, name_table: str, column_count: int) -> None:
+    count = '%s ' * column_count
+    with psycopg2.connect(
+            host='localhost',
+            database='north',
+            user='postgres',
+            password=PASS
+    ) as conn:
+        with conn.cursor() as cur:
+            for data_info in data_list:
+                val = tuple(data_info.values())
+                cur.execute(f"INSERT INTO {name_table} VALUES ({', '.join(count.split())})", val)
+                print(val)
+    conn.close()
+
+
+data_list = open_data_file(DATA_EMPLOYEES)
+add_data_to_database(data_list, 'employees', 6)
