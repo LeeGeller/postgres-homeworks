78788db79e3b4d803a79ab023401432ed09995be
Kristina Arkadievna <108188624+LeeGeller@users.noreply.github.com>
2024-02-21 20:28:26 +0300

    Created open_data_file()

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,15 @@
+import csv
+
+import psycopg2
+
+from config import DATA_EMPLOYEES
+
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+
+def open_data_file(data):
+    with open(data, encoding='utf-8') as file:
+        return csv.DictReader(file.read())
+
+
+print(open_data_file(DATA_EMPLOYEES))
